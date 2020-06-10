7f633ad090350766f1b797c2bf221a3b145c6610
leonardoj6 <l.jun1717@gmail.com>
2020-06-09 18:08:02 -0600

    refactor GroupSelector to functional component to use context hook

--- client/src/components/GroupSelector.js
+++ client/src/components/GroupSelector.js
@@ -1,12 +1,107 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
-import { selectGroup, addGroup } from "../actions";
+// import { selectGroup, addGroup } from "../actions";
 import { SelectedGroupContext } from "./SelectedGroupContext";
 import CreateGroup from "./CreateGroup";
 import { URL_ADD_GROUP } from "../config/urls";
 import Modal from "react-modal";
 Modal.setAppElement("#root");
+
+export default function GroupSelector(props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [groupNameInput, setGroupNameInput] = useState("");
+  const [selectedGroupId, setSelectedGroupId] = useContext(
+    SelectedGroupContext
+  );
+
+  useEffect(() => {
+    if (!selectedGroupId && props.groups.length > 0) {
+      setSelectedGroupId(props.groups[0]._id);
+    }
+  }, [selectedGroupId, props.groups]);
+
+  function openModal() {
+    setIsModalOpen(true);
+  }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
+
+  function onGroupNameInputChange(e) {
+    if (e.target.value) {
+      const text = e.target.value.trim();
+      setGroupNameInput(text);
+    }
+  }
+
+  function onAddNewGroup(e) {
+    e.preventDefault();
+    axios
+      .post(URL_ADD_GROUP, {
+        groupName: groupNameInput,
+        timeCreated: new Date(),
+      })
+      .then((response) => {
+        console.log(response);
+        setSelectedGroupId(response.data._id);
+        props.setCount((prevCount) => prevCount + 1);
+      });
+
+    closeModal();
+    setGroupNameInput("");
+    // this.setState({ isModalOpen: false, groupNameInput: "" });
+  }
+
+  function renderGroupList(groupList, currentGroupId) {
+    return groupList.map((group) => {
+      return (
+        <li
+          key={group._id}
+          className={
+            group._id === currentGroupId ? "group-selector_li--selected" : null
+          }
+          onClick={() => {
+            setSelectedGroupId(group._id);
+          }}
+        >
+          {group.groupName}
+        </li>
+      );
+    });
+  }
+
+  return (
+    <div className="group-selector">
+      <h4>Group</h4>
+      <ul>{renderGroupList(props.groups, selectedGroupId)}</ul>
+      <CreateGroup onHandleClick={openModal} />
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        overlayClassName="modal-overlay"
+        className="modal-content"
+      >
+        <div>
+          <header>
+            <h3>Add Group</h3>
+            <button onClick={closeModal}>x</button>
+          </header>
+          <form onSubmit={onAddNewGroup}>
+            <label>Group Name: </label>
+            <input onChange={onGroupNameInputChange} type="text"></input>
+          </form>
+          <footer>
+            <button onClick={onAddNewGroup}>Add</button>
+          </footer>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+/*
 
 class GroupSelector extends React.Component {
   constructor(props) {
@@ -51,7 +146,6 @@ class GroupSelector extends React.Component {
 
   renderGroupList(groupList, currentGroupId) {
     return groupList.map((group) => {
-      console.log(`selected group id: ${currentGroupId}`);
       return (
         <li
           key={group._id}
@@ -70,10 +164,10 @@ class GroupSelector extends React.Component {
 
   onAddNewGroup() {
     //redux
-    const newGroupId = this.props.addGroup(this.state.groupNameInput).payload
-      .groupId;
+    // const newGroupId = this.props.addGroup(this.state.groupNameInput).payload
+    //   .groupId;
 
-    this.props.selectGroup(newGroupId);
+    // this.props.selectGroup(newGroupId);
 
     // api
     axios
@@ -132,15 +226,6 @@ class GroupSelector extends React.Component {
   }
 }
 
-// function mapStateToProps(state) {
-//   return {
-//     groupSelected: state.groupSelected,
-//     groupList: state.groupList,
-//   };
-// }
-
-// export default connect(mapStateToProps)(
-//   GroupSelector
-// );
-
 export default GroupSelector;
+
+*/
--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -5,10 +5,10 @@ import { Redirect, Route } from "react-router-dom";
 class PrivateRoute extends React.Component {
   render() {
     console.log(this.props);
-    const { component: Main, path, ...rest } = this.props;
+    const { component: PrivateComponent, path, ...rest } = this.props;
     return (
       <Route path={path} {...rest}>
-        {this.props.authed ? <Main /> : <Redirect to="/login" />}
+        {this.props.authed ? <PrivateComponent /> : <Redirect to="/login" />}
       </Route>
     );
   }
