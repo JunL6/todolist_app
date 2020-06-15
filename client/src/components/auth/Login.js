cbd86940bf06d23403600e86f2f87612dfa7bd08
leonardoj6 <l.jun1717@gmail.com>
2020-06-15 01:39:57 -0600

    finished Login.js styling and feedback functionalities

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,44 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Container,
+  Row,
+  Col,
+  NavDropdown,
+} from "react-bootstrap";
 
 import { URL_LOGIN } from "../../config/urls";
 import { authUser } from "../../actions";
 import "./Login.css";
+import logo from "../../img/Todoie_logo.svg";
 
-class Login extends React.Component {
-  constructor(props) {
-    // console.log(props);
-    super(props);
-    this.onFormSubmit = this.onFormSubmit.bind(this);
-  }
+function Login(props) {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [validated, setValidated] = useState(false);
+  const [isCredentialCorrect, setIsCredentialCorrect] = useState(false);
 
-  onFormSubmit = (formProps) => {
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+    event.preventDefault();
+    const formProps = { username, password };
     axios
-      .post(
-        URL_LOGIN,
-        formProps
-        // { withCredentials: true }
-      )
+      .post(URL_LOGIN, formProps)
       .then((response) => {
         console.log(response);
         if (response.status === 200) {
-          this.props.authUser(JSON.parse(response.config.data).username);
-          console.log(this.props.authed);
-          this.props.history.push("/app");
+          props.authUser(JSON.parse(response.config.data).username);
+          console.log(props.authed);
+          props.history.push("/app");
         }
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        console.error(err);
+
+        setIsCredentialCorrect(true);
+        setPassword(null);
+      });
   };
 
-  render() {
-    const { handleSubmit } = this.props;
-    return (
-      <div className="login bg-ligth">
-        {/* <form onSubmit={handleSubmit(this.onFormSubmit)}>
+  // const { handleSubmit } = this.props;
+  return (
+    <div className="login bg-light">
+      {/* <form onSubmit={handleSubmit(this.onFormSubmit)}>
           <fieldset>
           <label>Email:</label>
           <Field name="username" component="input" type="text" />
@@ -49,41 +65,66 @@ class Login extends React.Component {
           </fieldset>
           <button>Log in</button>
         </form> */}
-        <Container fluid>
-          <Row className="pt-5 justify-content-center align-items-center">
-            <h3 className="">Log in</h3>
-          </Row>
-          <Row className="my-4 justify-content-center">
-            <Col className="login-form-container pt-3 pb-4 px-4 border rounded">
-              <Form>
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label className="font-weight-bold">Email</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
+      <Container fluid>
+        <Row className="pt-5 justify-content-center align-items-center">
+          <Link to="/" className="d-flex align-items-end logo-link">
+            <img src={logo} width="30" height="30" />
+            <h3 className="d-inline-block m-0 text-dark">odoie</h3>
+          </Link>
+        </Row>
+        <Row className="my-4 justify-content-center">
+          <Col className="login-form-container pt-3 pb-4 px-4 border rounded">
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+              <h3 className="text-center text-secondary">Log in</h3>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label className="font-weight-bold">Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  required
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a proper Email.
+                </Form.Control.Feedback>
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword" className="mb-4">
+                <Form.Label className="font-weight-bold">Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a proper password.
+                </Form.Control.Feedback>
+              </Form.Group>
 
-                <Form.Group controlId="formBasicPassword" className="mb-4">
-                  <Form.Label className="font-weight-bold">Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit" block>
-                  Submit
-                </Button>
-                <div className="clearfix"></div>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
-
-        <Link to="/app">App</Link>
-      </div>
-    );
-  }
+              <Button variant="primary" type="submit" block>
+                Submit
+              </Button>
+              <div
+                className="text-danger"
+                style={{ display: isCredentialCorrect ? "block" : "none" }}
+              >
+                Invalid username or password
+              </div>
+            </Form>
+            <NavDropdown.Divider />
+            <div className="text-muted">
+              Don't have an account yet? <Link to="/signup">Sign up</Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => ({ authed: state.authed });
 const mapDispatchToProps = { authUser };
-const connectedComponent = connect(mapStateToProps, mapDispatchToProps)(Login);
-export default reduxForm({ form: "login" })(connectedComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
