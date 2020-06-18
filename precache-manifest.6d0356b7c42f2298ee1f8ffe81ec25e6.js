d68950a9d6ab78931b6d4ceae63aadec5915cebd
leonardoj6 <l.jun1717@gmail.com>
2020-06-18 02:38:49 -0600

    Updates

---
+++ precache-manifest.6d0356b7c42f2298ee1f8ffe81ec25e6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aabb7bc78fdd66097413f6a3725f4c4d",
+    "url": "/todolist_app/index.html"
+  },
+  {
+    "revision": "27790101b1128e4cd0d8",
+    "url": "/todolist_app/static/css/main.f4bbc047.chunk.css"
+  },
+  {
+    "revision": "bbe3f48deca201ae32f6",
+    "url": "/todolist_app/static/js/2.b8eca05a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todolist_app/static/js/2.b8eca05a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27790101b1128e4cd0d8",
+    "url": "/todolist_app/static/js/main.c020248a.chunk.js"
+  },
+  {
+    "revision": "f58df653fd02a023aaa7",
+    "url": "/todolist_app/static/js/runtime-main.04fe9bef.js"
+  },
+  {
+    "revision": "6a235456462ff339ef3bd148d73b144e",
+    "url": "/todolist_app/static/media/Todoie_logo.6a235456.svg"
+  },
+  {
+    "revision": "cb7b7a58ebb9271c1132e116a6b43766",
+    "url": "/todolist_app/static/media/landingpage-background.cb7b7a58.jpg"
+  },
+  {
+    "revision": "183dc3a68eeb7183715704b612a0fdea",
+    "url": "/todolist_app/static/media/logo.183dc3a6.svg"
+  },
+  {
+    "revision": "c135dbbd2d2a74ce25ef966dc3e936d4",
+    "url": "/todolist_app/static/media/todolist_item--completed.c135dbbd.svg"
+  },
+  {
+    "revision": "74254da3457f073745d11ab5b5b60828",
+    "url": "/todolist_app/static/media/todolist_item--incomplete.74254da3.svg"
+  }
+]);
