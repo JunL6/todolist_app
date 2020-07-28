7ec1d0580d6fed3874c012576a9ad4e11dea1a42
kiranbandi <bvenkatkiran@gmail.com>
2020-07-27 22:49:50 -0600

    Updates

---
+++ precache-manifest.425bf833d9c57b9b71a9264313c5f7a9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a198428a19581f8fa5d866a1e85426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61e862f865b88fd38dc0",
+    "url": "/static/css/main.f4001269.chunk.css"
+  },
+  {
+    "revision": "0781de8c05e600679fa8",
+    "url": "/static/js/2.344026b5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.344026b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e862f865b88fd38dc0",
+    "url": "/static/js/main.c18e23f3.chunk.js"
+  },
+  {
+    "revision": "2189ad404f108a6437fb",
+    "url": "/static/js/runtime-main.f141b71d.js"
+  },
+  {
+    "revision": "6a235456462ff339ef3bd148d73b144e",
+    "url": "/static/media/Todoie_logo.6a235456.svg"
+  },
+  {
+    "revision": "cb7b7a58ebb9271c1132e116a6b43766",
+    "url": "/static/media/landingpage-background.cb7b7a58.jpg"
+  },
+  {
+    "revision": "183dc3a68eeb7183715704b612a0fdea",
+    "url": "/static/media/logo.183dc3a6.svg"
+  },
+  {
+    "revision": "c135dbbd2d2a74ce25ef966dc3e936d4",
+    "url": "/static/media/todolist_item--completed.c135dbbd.svg"
+  },
+  {
+    "revision": "74254da3457f073745d11ab5b5b60828",
+    "url": "/static/media/todolist_item--incomplete.74254da3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todolist_app/precache-manifest.5f6f9d9f180b1e9b7e7547f1e5bf5a4c.js"
+  "/precache-manifest.425bf833d9c57b9b71a9264313c5f7a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todolist_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
