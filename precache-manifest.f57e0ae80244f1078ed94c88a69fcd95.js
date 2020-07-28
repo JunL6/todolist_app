fb134ceaab029ea7b480ee882ab9c3ada49faad2
kiranbandi <bvenkatkiran@gmail.com>
2020-07-27 22:58:52 -0600

    Updates

---
+++ precache-manifest.f57e0ae80244f1078ed94c88a69fcd95.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381c91013fee0ba180a4d9f869e64653",
+    "url": "/todolist_app/index.html"
+  },
+  {
+    "revision": "1b7b9d3501da8c93688c",
+    "url": "/todolist_app/static/css/main.da506878.chunk.css"
+  },
+  {
+    "revision": "0781de8c05e600679fa8",
+    "url": "/todolist_app/static/js/2.344026b5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todolist_app/static/js/2.344026b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7b9d3501da8c93688c",
+    "url": "/todolist_app/static/js/main.c18e23f3.chunk.js"
+  },
+  {
+    "revision": "f58df653fd02a023aaa7",
+    "url": "/todolist_app/static/js/runtime-main.04fe9bef.js"
+  },
+  {
+    "revision": "6a235456462ff339ef3bd148d73b144e",
+    "url": "/todolist_app/static/media/Todoie_logo.6a235456.svg"
+  },
+  {
+    "revision": "cb7b7a58ebb9271c1132e116a6b43766",
+    "url": "/todolist_app/static/media/landingpage-background.cb7b7a58.jpg"
+  },
+  {
+    "revision": "183dc3a68eeb7183715704b612a0fdea",
+    "url": "/todolist_app/static/media/logo.183dc3a6.svg"
+  },
+  {
+    "revision": "c135dbbd2d2a74ce25ef966dc3e936d4",
+    "url": "/todolist_app/static/media/todolist_item--completed.c135dbbd.svg"
+  },
+  {
+    "revision": "74254da3457f073745d11ab5b5b60828",
+    "url": "/todolist_app/static/media/todolist_item--incomplete.74254da3.svg"
+  }
+]);
