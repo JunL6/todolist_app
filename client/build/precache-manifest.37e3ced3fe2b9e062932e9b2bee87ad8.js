8052f587f0ea9b46e7c44ac03bc9f2df6dc30333
leonardoj6 <l.jun1717@gmail.com>
2020-08-21 19:10:39 -0600

    changed url to heroku server

---
+++ client/build/precache-manifest.37e3ced3fe2b9e062932e9b2bee87ad8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f75269fc23ac5e290a354cbb1dcfdb",
+    "url": "/todolist_app/index.html"
+  },
+  {
+    "revision": "2112bd76b0b8628b481c",
+    "url": "/todolist_app/static/css/main.179fa7ee.chunk.css"
+  },
+  {
+    "revision": "b63461912feba89569c8",
+    "url": "/todolist_app/static/js/2.2fcfedcf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todolist_app/static/js/2.2fcfedcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2112bd76b0b8628b481c",
+    "url": "/todolist_app/static/js/main.be4203b6.chunk.js"
+  },
+  {
+    "revision": "f58df653fd02a023aaa7",
+    "url": "/todolist_app/static/js/runtime-main.04fe9bef.js"
+  },
+  {
+    "revision": "6a235456462ff339ef3bd148d73b144e",
+    "url": "/todolist_app/static/media/Todoie_logo.6a235456.svg"
+  },
+  {
+    "revision": "cb7b7a58ebb9271c1132e116a6b43766",
+    "url": "/todolist_app/static/media/landingpage-background.cb7b7a58.jpg"
+  },
+  {
+    "revision": "183dc3a68eeb7183715704b612a0fdea",
+    "url": "/todolist_app/static/media/logo.183dc3a6.svg"
+  },
+  {
+    "revision": "c135dbbd2d2a74ce25ef966dc3e936d4",
+    "url": "/todolist_app/static/media/todolist_item--completed.c135dbbd.svg"
+  },
+  {
+    "revision": "74254da3457f073745d11ab5b5b60828",
+    "url": "/todolist_app/static/media/todolist_item--incomplete.74254da3.svg"
+  }
+]);
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
   app.use(
     ["/api/*"],
     createProxyMiddleware({
-      // target: "http://localhost:4000",
-      target: "https://todoie.herokuapp.com",
+      target: "http://localhost:4000",
+      // target: "https://todoie.herokuapp.com",
       changeOrigin: true,
     })
   );
