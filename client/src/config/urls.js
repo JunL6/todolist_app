2431bf3ea48ca6bb83a126f93410079642d9eeb0
leonardoj6 <l.jun1717@gmail.com>
2021-01-15 02:52:53 -0600

    initial commit for this branch using hook, replacing redux

--- client/src/config/urls.js
+++ client/src/config/urls.js
@@ -1,5 +1,5 @@
-var endpointRoot = "https://todoie.herokuapp.com";
-// const endpointRoot = "";
+// const endpointRoot = "https://todoie.herokuapp.com";
+const endpointRoot = "";
 export const URL_FETCH_USER = endpointRoot + "/api/current_user";
 export const URL_LOGIN = endpointRoot + "/api/login";
 export const URL_LOGOUT = endpointRoot + "/api/logout";
